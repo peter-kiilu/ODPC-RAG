294293ee807409eafc1b38adec48a45b9417ee06
Rickmwasofficial <rickmwasofficial@gmail.com>
2026-01-28 15:54:22 +0300

    fix issue logs

--- frontend/services/apiService.ts
+++ frontend/services/apiService.ts
@@ -2,7 +2,7 @@
 import { ChatRequest, ChatResponse, HealthStatus } from '../types';
 
 // EDIT THIS BASE URL TO YOUR ACTUAL BACKEND LINK
-const BASE_URL = 'https://8000-w-rickmwasofficial-mkpfbulw.cluster-s5xdz26smvgniwoeurkaozovss.cloudworkstations.dev'; 
+const BASE_URL = 'http://localhost:8000'; 
 
 export const apiService = {
   async sendMessage(message: string): Promise<ChatResponse> {
